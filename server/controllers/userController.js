afb148d781df98e590ffacf8e43ad2c0452d012c
Evans Koech <biwottech@gmail.com>
2024-07-27 21:35:24 +0300

    working on authentication using jsonwebtokens

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -41,6 +41,7 @@ const forgotPassword = async (req, res) => {
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
+
     await PasswordReset.create({
       email: user.email,
       token,
@@ -105,4 +106,13 @@ const verifyOtp = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+};
+
+module.exports = {
+  register,
+  login,
+  confirmEmail,
+  forgotPassword,
+  resetPassword,
+  verifyOtp,
 };
